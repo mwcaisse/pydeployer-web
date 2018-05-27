588854fb7f10f44d8b759a3eadf0a31f70efd43f
Mitchell Caisse <computers1055@gmail.com>
2018-05-27 19:23:29 -0400

    Added implementation of ApplicationEnvironmentService

---
+++ PyDeployer/PyDeployer.Logic/Services/ApplicationEnvironmentService.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mitchell.Common;
+using Mitchell.Common.Exceptions;
+using PyDeployer.Common.Entities;
+using PyDeployer.Data;
+
+namespace PyDeployer.Logic.Services
+{
+    public class ApplicationEnvironmentService
+    {
+
+        private readonly PyDeployerDbContext _db;
+
+        public ApplicationEnvironmentService(PyDeployerDbContext db)
+        {
+            this._db = db;
+        }
+
+        public ApplicationEnvironment Get(long id)
+        {
+            return _db.ApplicationEnvironments.Active().FirstOrDefault(ae => ae.ApplicationEnvironmentId == id);
+        }
+
+        public IEnumerable<ApplicationEnvironment> GetForApplication(long applicationId)
+        {
+            return _db.ApplicationEnvironments.Active().Where(ae => ae.ApplicationId == applicationId);
+        }
+
+        public ApplicationEnvironment Create(long applicationId, long environmentId)
+        {
+            if (!ApplicationExists(applicationId))
+            {
+                throw new EntityValidationException("Application does not exist!");
+            }
+            if (!EnvironmentExists(environmentId))
+            {
+                throw new EntityValidationException("Environment does not exist!");
+            }
+
+            if (
+                _db.ApplicationEnvironments.Active()
+                    .Any(ae => ae.ApplicationId == applicationId && ae.EnvironmentId == environmentId))
+            {
+                throw new EntityValidationException("This environment is already added to this application.");
+            }
+
+            var env = new ApplicationEnvironment()
+            {
+                ApplicationId = applicationId,
+                EnvironmentId =  environmentId,
+                Active = true
+            };
+
+            _db.ApplicationEnvironments.Add(env);
+            _db.SaveChanges();
+
+            return env;
+        }
+
+        public bool Delete(long applicationId, long environmentId)
+        {
+            var env =
+                _db.ApplicationEnvironments.Active()
+                    .FirstOrDefault(ae => ae.ApplicationId == applicationId && ae.EnvironmentId == environmentId);
+
+            if (null == env)
+            {
+                throw new EntityValidationException("This enviroment is not added to this application");
+            }
+
+            env.Active = false;
+            _db.SaveChanges();
+
+            return true;
+        }
+
+        protected ApplicationEnvironment GetOrException(long id,
+            string exceptionText = "Application Environment doesn't exist")
+        {
+            var environment =
+                _db.ApplicationEnvironments.Active().FirstOrDefault(ae => ae.ApplicationEnvironmentId == id);
+
+            if (null == environment)
+            {
+                throw new EntityValidationException(exceptionText);
+            }
+
+            return environment;
+        }
+
+        protected bool ApplicationExists(long applicationId)
+        {
+            return _db.Applications.Active().Any(a => a.ApplicationId == applicationId);
+        }
+
+        protected bool EnvironmentExists(long environmentId)
+        {
+            return _db.Environments.Active().Any(e => e.EnvironmentId == environmentId);
+        }
+
+    }
+}
--- PyDeployer/PyDeployer.Web/Controllers/Api/ApplicationEnvironmentApiController.cs
+++ PyDeployer/PyDeployer.Web/Controllers/Api/ApplicationEnvironmentApiController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PyDeployer.Logic.Services;
 
 namespace PyDeployer.Web.Controllers.Api
 {
@@ -11,26 +12,35 @@ namespace PyDeployer.Web.Controllers.Api
     [Route("api/application/{applicationId:long}/environment/")]
     public class ApplicationEnvironmentApiController : Controller
     {
-        
+
+        private readonly ApplicationEnvironmentService _applicationEnvironmentService;
+
+        public ApplicationEnvironmentApiController(ApplicationEnvironmentService applicationEnvironmentService)
+        {
+            this._applicationEnvironmentService = applicationEnvironmentService;
+        }
+
         [HttpGet]
         [Route("")]
         public IActionResult Get(long applicationId)
         {
-            return Ok();
+            return Ok(_applicationEnvironmentService.GetForApplication(applicationId)
+                .Select(ae => ae.Environment).ToList());
         }
 
         [HttpPost]
         [Route("{environmentId:long}")]
         public IActionResult Create(long applicationId, long environmentId)
         {
-            return Ok();
+            _applicationEnvironmentService.Create(applicationId, environmentId);
+            return Ok(true);
         }
 
         [HttpDelete]
         [Route("{environmentId:long}")]
         public IActionResult Delete(long applicationId, long environmentId)
         {
-            return Ok();
+            return Ok(_applicationEnvironmentService.Delete(applicationId, environmentId));
         }
 
     }

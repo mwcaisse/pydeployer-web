4a73c9d4abe13f8b2e0399b8077b3554ead4775b
Mitchell Caisse <computers1055@gmail.com>
2018-05-27 11:45:13 -0400

    Added an environment service

---
+++ PyDeployer/PyDeployer.Logic/Services/EnvironmentService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mitchell.Common.Exceptions;
+using PyDeployer.Common.ViewModels;
+using PyDeployer.Data;
+
+namespace PyDeployer.Logic.Services
+{
+    public class EnvironmentService
+    {
+
+        private readonly PyDeployerDbContext _db;
+
+        public EnvironmentService(PyDeployerDbContext db)
+        {
+            this._db = db;
+        }
+
+
+        public Common.Entities.Environment Get(long id)
+        {
+            return _db.Environments.FirstOrDefault(e => e.EnvironmentId == id);
+        }
+
+        public Common.Entities.Environment GetByUuid(string uuid)
+        {
+            var guid = new Guid(uuid);
+            return _db.Environments.FirstOrDefault(e => e.EnvironmentUuid == guid);
+        }
+
+        public IEnumerable<Common.Entities.Environment> GetAll()
+        {
+            return _db.Environments.ToList();
+        }
+
+        public Common.Entities.Environment Create(EnvironmentViewModel toCreate)
+        {
+            if (string.IsNullOrEmpty(toCreate.Name))
+            {
+                throw new EntityValidationException("Environmemnt Name cannot be blank!");
+            }
+
+            var env = new Common.Entities.Environment()
+            {
+                Name = toCreate.Name,
+                HostName = toCreate.HostName,
+                EnvironmentUuid = Guid.NewGuid(),
+                Active = true
+            };
+
+            _db.SaveChanges();
+
+            return env;
+        }
+
+        public Common.Entities.Environment Update(EnvironmentViewModel toUpdate)
+        {
+            var env = Get(toUpdate.EnvironmentId);
+
+            if (string.IsNullOrEmpty(toUpdate.Name))
+            {
+                throw new EntityValidationException("Environmemnt Name cannot be blank!");
+            }
+
+            env.Name = toUpdate.Name;
+            env.HostName = toUpdate.HostName;
+
+            return env;
+        }
+
+        public bool Delete(long id)
+        {
+            var env = GetOrException(id);
+
+            env.Active = false;
+            _db.SaveChanges();
+
+            return true;
+        }
+
+        protected Common.Entities.Environment GetOrException(long id,
+            string exceptionText = "Environment does not exist!")
+        {
+            var env = Get(id);
+
+            if (null == env)
+            {
+                throw new EntityValidationException(exceptionText);
+            }
+
+            return env;
+        }
+
+    }
+}
--- PyDeployer/PyDeployer.Web/Controllers/Api/EnvironmentApiController.cs
+++ PyDeployer/PyDeployer.Web/Controllers/Api/EnvironmentApiController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PyDeployer.Common.ViewModels;
+using PyDeployer.Logic.Services;
 
 namespace PyDeployer.Web.Controllers.Api
 {
@@ -13,46 +14,54 @@ namespace PyDeployer.Web.Controllers.Api
     public class EnvironmentApiController : Controller
     {
 
+        private readonly EnvironmentService _environmentService;
+
+        public EnvironmentApiController(EnvironmentService environmentService)
+        {
+            this._environmentService = environmentService;
+        }
+
         [HttpGet]
         [Route("{id:long}")]
         public IActionResult Get(long id)
         {
-            return Ok();
+            return Ok(_environmentService.Get(id));
         }
 
         [HttpGet]
         [Route("{uuid:string}")]
         public IActionResult GetByUuid(string uuid)
         {
-            return Ok();
+            return Ok(_environmentService.GetByUuid(uuid));
         }
 
         [HttpGet]
         [Route("")]
         public IActionResult GetAll()
         {
-            return Ok();
+            return Ok(_environmentService.GetAll());
         }
 
         [HttpPost]
         [Route("")]
         public IActionResult Create([FromBody] EnvironmentViewModel environment)
         {
-            return Ok();
+            return Ok(_environmentService.Create(environment));
         }
 
         [HttpPut]
-        [Route("")]
-        public IActionResult Update([FromBody] EnvironmentViewModel environment)
+        [Route("{id:long}")]
+        public IActionResult Update(long id, [FromBody] EnvironmentViewModel environment)
         {
-            return Ok();
+            environment.EnvironmentId = id;
+            return Ok(_environmentService.Update(environment));
         }
 
         [HttpDelete]
         [Route("{id:long}")]
         public IActionResult Delete(long id)
         {
-            return Ok();
+            return Ok(_environmentService.Delete(id));
         }
 
     }

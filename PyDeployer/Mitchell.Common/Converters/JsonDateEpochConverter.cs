12de26553ac6b6c623796a94259d1de01dc517cc
Mitchell <computers1055@gmail.com>
2018-06-04 08:48:35 -0400

    Added basic configuration file

---
+++ PyDeployer/Mitchell.Common/Converters/JsonDateEpochConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Mitchell.Common.Converters
+{
+    public class JsonDateEpochConverter : JsonConverter
+    {
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var date = value as DateTime?;
+            if (date.HasValue)
+            {
+                long milliseconds = new DateTimeOffset(date.Value.ToUniversalTime()).ToUnixTimeMilliseconds();
+                writer.WriteRawValue(milliseconds.ToString());
+            }
+            else
+            {
+                writer.WriteNull();
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var stringVal = reader.Value.ToString();
+            if (string.IsNullOrWhiteSpace(stringVal))
+            {
+                return null;
+            }
+            long milliseconds = Convert.ToInt64(stringVal);
+            return DateTimeOffset.FromUnixTimeMilliseconds(milliseconds).DateTime.ToLocalTime();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(DateTime) || objectType == typeof(DateTime?);
+        }
+    }
+}
--- PyDeployer/PyDeployer.Web/Startup.cs
+++ PyDeployer/PyDeployer.Web/Startup.cs
@@ -4,18 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Mitchell.Common.Converters;
+using PyDeployer.Data;
+using PyDeployer.Logic.Services;
+using PyDeployer.Web.Converters;
 
 namespace PyDeployer.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("dbConfig.json");
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -23,18 +32,52 @@ namespace PyDeployer.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddOptions();
+
+            services.AddDbContext<PyDeployerDbContext>(options =>
+                options.UseMySql(Configuration.GetSection("connectionString").Value)
+            );
+
+            services.AddTransient<ApplicationService>();
+            services.AddTransient<ApplicationTokenService>();
+            services.AddTransient<EnvironmentService>();
+            services.AddTransient<ApplicationEnvironmentService>();
+            services.AddTransient<ApplicationEnvironmentTokenService>();
+
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                //View Model serializers for entities
+                options.SerializerSettings.Converters.Add(new JsonApplicationConverter());
+                options.SerializerSettings.Converters.Add(new JsonApplicationEnvironmentTokenConverter());
+                options.SerializerSettings.Converters.Add(new JsonApplicationTokenConverter());
+                options.SerializerSettings.Converters.Add(new JsonEnvironmentConverter());
+
+                //General Serializers
+                options.SerializerSettings.Converters.Add(new JsonDateEpochConverter());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            app.UseStaticFiles();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
 
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }

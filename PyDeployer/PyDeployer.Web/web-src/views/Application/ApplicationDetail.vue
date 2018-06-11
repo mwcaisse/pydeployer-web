684fb133428c4df29c044c687d097a067872cda5
Mitchell <computers1055@gmail.com>
2018-06-11 08:44:07 -0400

    Created skeleton for application details

--- PyDeployer/PyDeployer.Web/web-src/views/Application/ApplicationDetail.vue
+++ PyDeployer/PyDeployer.Web/web-src/views/Application/ApplicationDetail.vue
@@ -1,7 +1,42 @@
 ﻿<template>
     <div>
-        <h2>Application Details</h2>
-    </div>
+        <section class="section">
+            <div class="container">
+                <h1 class="title">Application Details</h1>
+                <p class="subtitle">
+                    Details of Test Application 1
+                </p>
+            </div>
+        </section>
+        <div class="columns">
+            <div class="column">
+                <div class="box">
+                    <div class="field">
+                        <label class="label">Name</label>
+                        <div class="control">
+                            <span>Test Application 1</span>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label class="label">UUID</label>
+                        <div class="control">
+                            <span>UUID GOES HERE</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="box">
+                    <h2>Environments</h2>
+                    <p>A list/grid of the environments that this application is associated with</p>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <h2>Tokens</h2>
+            <p>A list of all the Tokens that have been defined for this application.</p>
+        </div>
+    </div> 
 </template>
 
 <script>
